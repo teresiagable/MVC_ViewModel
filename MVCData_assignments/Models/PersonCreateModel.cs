34aec8a3b58d7c089c18e6c8f948dd1ee08d687e
Teresia Gable <45754965+teresiagable@users.noreply.github.com>
2020-02-06 14:12:34 +0100

    MVC Data Assignment 1 completed

--- MVCData_assignments/Models/PersonCreateModel.cs
+++ MVCData_assignments/Models/PersonCreateModel.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace MVCData_assignments.Models
 {
-    public class PersonViewModel
+    public class PersonCreateModel
     {
         [Required]
         [StringLength(32, ErrorMessage = "The name is too long", MinimumLength = 1)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(32, ErrorMessage = "The city name is too long", MinimumLength = 1)]
         public string City { get; set; }
         public string Phonenumber { get; set; }
 
---
+++ MVCData_assignments/Models/PersonListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCData_assignments.Models
+{
+    public class PersonListViewModel
+    {
+        public List<Person> PersonList { get; set; }
+        public string Filter { get; set; }
+    }
+}
